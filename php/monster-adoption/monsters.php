4aee2260370d1a9940799628025b1f293f5bfd9c
Jesse <jessemerge2@gmail.com>
2022-05-27 01:13:52 -0700

    adding object to array trials

--- php/monster-adoption/monsters.php
+++ php/monster-adoption/monsters.php
@@ -94,10 +94,12 @@ function monsterGenerator($name, $favoriteFood, $age, $adopted, $portrait, $hue,
     ];
 
     return $monster;
-    $monsters.push($monster);
-
 }
 
+$monsters[]= $monster;
+
 $chunky = monsterGenerator('Chunky','coconuts', 12, false, 'https://static.wikia.nocookie.net/donkeykong/images/8/87/ChunkyKongDK64Image.jpg/revision/latest?cb=20080614061025', .6, 'play baseball' )
+
+
 
 ?>
