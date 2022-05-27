59bb60a231aba12427ed0998ee9a06cb9976b44a
Jesse <jessemerge2@gmail.com>
2022-05-26 23:32:26 -0700

    changed alpha net to php

---
+++ php/monster-practice/monster-factory.php
@@ -0,0 +1,31 @@
+<?php
+$marshall =[
+        "id" => 001,
+        "name" => "Marshall",
+        "favoriteFood" => "Doritos",
+        "age" => 9,
+        "adopted" => true,
+        "portrait" => "https://peprojects.dev/images/portrait.jpg",
+    ];
+    
+    $dom =[
+        "id" => 002,
+        "name" => "Dom",
+        "favoriteFood" => "Cookies",
+        "age" => 9,
+        "adopted" => false,
+        "portrait" => "https://peprojects.dev/images/portrait.jpg",
+    ];
+
+    $uly =[
+        "id" => 003,
+        "name" => "Uly",
+        "favoriteFood" => "Red vines",
+        "age" => 5,
+        "adopted" => false,
+        "portrait" => "https://peprojects.dev/images/portrait.jpg",
+    ];
+
+    $monsters = [$marshall, $dom, $uly];
+
+    ?>
---
+++ php/monster-practice/style.php
@@ -0,0 +1,37 @@
+<style>
+    p{
+        font-size: 1.3rem;
+    }
+
+    .red{
+        color:red;
+    }
+
+    .blue{
+        color:blue;
+    }
+
+    section.one {
+        background-color: hsl(0,0%,0%);
+        color:white;
+        padding:20px;
+    }
+    
+    section.two {
+        background-color: hsl(100,0%,90%);
+        color:black;
+        padding:20px;
+    }
+
+    ol{
+        display:flex;
+        flex-direction:row;
+        list-style: none;
+        padding:0;
+    }
+
+    div.monster-card {
+        padding: 20px;
+        border: 2px solid red
+    }
+</style>
