8d8934db8003543f2b17c2314eeab5ede841819c
Jesse <jessemerge2@gmail.com>
2022-06-11 02:47:19 -0700

    Changing button styling

--- connect.php
+++ connect.php
@@ -5,6 +5,6 @@
     </article>
     <article class="three">
         <p>Interested in my journey? Hear me talk about things that I <em>enjoy</em>, that <em>challenge me</em>, and <em>what's next!</em> </p>
-        <a target="_blank" href="https://jessedevs.substack.com/publish?utm_source=menu">My Blog</a>
+        <a target="_blank" href="https://jessedevs.substack.com/">My Blog</a>
     </article>
 </article-grid>
