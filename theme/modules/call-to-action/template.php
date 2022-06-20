c6f561f5a0ad87ab7f09c57211c332e1139599ec
Jesse <jessemerge2@gmail.com>
2022-06-20 15:03:54 -0700

    random file delete

--- theme/modules/call-to-action/template.php
+++
@@ -1,7 +0,0 @@
-<call-to-action>
-	<h2 class='heading attention-voice'>Another Story</h2>
-
-	<p class='story'>Bradley Beal Says He's Made Decision on Future with Wizards, Gives Injury Update.</p>
-
-	<a class='button' href='#'>Read More</a>
-</call-to-action>
