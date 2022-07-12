040ccfc9496981dd3e7ba09e897d22edd569d00c
Jesse <jessemerge2@gmail.com>
2022-07-12 00:08:34 -0700

    udpate goals

--- goals.php
+++ goals.php
@@ -5,18 +5,20 @@
 $goals = [
     'week' => [
         'title' => 'week...',
-        'one' => 'Continue practicing PHP and testing my current skills.',
-        'two' => 'Write more blog post starting with one about PHP introduction.',
-        'three' => 'Start making graphics.',
+        'one' => 'Use PHP and Wordpress CMS to work on some old and new projects. This would give me some good pratice and create some portfolio ready projects.',
+        'two' => 'Write more blog post, specifically talking about the PHP CRUD project',
+        'three' => 'Start making graphics. I want to look into blender and make some 3D models.',
         'four' => 'Keep an eye on my portfolio page and search for external feedback.',
         'five' => 'Meet with Derek as much as possible.',
     ],
     'month' => [
         'title' => 'month...',
-        'one' => 'Practice some creativity and using a graphic design program by creating an image every day of the month.',
-        'two' => 'Be able to use PHP comfortably in most scenarios.',
-        'three' => 'Develop current projects for display on portfolio.',
-        'four' => 'Plan some meditation to avoid burnout.',
+        'one' => 'Prepare for incoming lessons about Javascript.',
+        'two' => 'Use all the tools we’ve gained so far to delelop new ideas and projects.',
+
+        'three' => 'Be able to use PHP comfortably in most scenarios.',
+        'four' => 'Develop current projects for display on portfolio.',
+        'five' => 'Plan some meditation to avoid burnout.',
 
     ],
     'course' => [
@@ -24,7 +26,7 @@ $goals = [
         'one' => 'I want to get an entry level job as a software engineer and or designer.',
         'two' => "I would love to make above 100k and be able to say I'm happy in my place of employment. The size of the company is still in question. I see benefits in all scenarios but I belive somewhere to get my feet wet and build a foundation may be best for the long run of my career.",
         'three' => "Be able to say I've learned so much and establish a valuble connection with everyone in the course.",
-        'four' => 'Continue to learn new skills. 3D graphics? Game Design? More interactive graphics?',
+        'four' => 'Continue to learn new skills. New frameworks? 3D graphics? Game Design? More interactive graphics?',
 
     ],
     'fiveYear' => [
