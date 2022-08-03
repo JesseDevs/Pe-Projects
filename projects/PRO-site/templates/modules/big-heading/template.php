8d33471403473d7f23b3de204c552017f2773216
Jesse <jessemerge2@gmail.com>
2022-08-03 16:38:41 -0700

    Working on landing page set up

--- projects/PRO-site/templates/modules/big-heading/template.php
+++ projects/PRO-site/templates/modules/big-heading/template.php
@@ -1,7 +1,8 @@
 <?php
-$heading = $section['heading'] ?? "Default big heading heading";
+$heading = $section['heading'] ?? ucfirst($page);
 ?>
 
 <big-heading>
     <h1 class="loud-voice"><?= $heading ?>!!</h1>
+    <img src=" <?= $section['image'] ?>"  alt="comp">
 </big-heading>
