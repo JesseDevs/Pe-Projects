41f230323faee561baa5c2d08e36d5a7caf16c51
Jesse <jessemerge2@gmail.com>
2022-08-04 10:43:44 -0700

    work on landing for site

--- projects/PRO-site/templates/modules/landing-heading/template.php
+++ projects/PRO-site/templates/modules/landing-heading/template.php
@@ -5,8 +5,12 @@ $heading = $section['heading'] ?? ucfirst($page);
 <landing-heading>
     <h1 class="roar-voice"><?= $heading ?></h1>
     <div class="intro"> <?= $section["intro"] ?></div>
-    <p class="details"><?= $section["details"] ?></p>
+
     <picture class="landing-image">
+        <p class="details"><?= $section["details"] ?></p>
         <img src=" <?= $section['image'] ?>"  alt="comp">
     </picture>
+    <svg class="arrows">
+        <path stroke-linecap="round" class="a1" d="M0 0 L20 22 L40 0"></path>
+    </svg>
 </landing-heading>
