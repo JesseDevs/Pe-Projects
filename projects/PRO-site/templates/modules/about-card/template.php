5965cb144a67b56851448a648f28401e60a3419c
Jesse <jessemerge2@gmail.com>
2022-08-06 16:01:37 -0700

    adjust about card layout and css

--- projects/PRO-site/templates/modules/about-card/template.php
+++ projects/PRO-site/templates/modules/about-card/template.php
@@ -1,13 +1,20 @@
 <about-card>
     <h2><?= $section['heading']; ?></h2>
-    <p><?= $section['intro']; ?></p>
-    <?php 
-    foreach ($section['details'] as $detail){ 
-        ?>
+    <text-content>
+        <p class='intro'><?= $section['intro']; ?></p>
+        <?php 
+        foreach ($section['details'] as $detail){ 
+            ?>
 
-        <p> <?= $detail ?></p>
-    <?php } ?>
+            <p> <?= $detail ?></p>
+        <?php } ?>
+
+        <?php foreach ($pageData['ad'] as $ad){ ?>
+            <a class="blog" href ='$ad["blog"]'><?= $ad["intro"]?></a>
+        <?php } ?>
+    </text-content>
 </about-card>
+
 
 
 
