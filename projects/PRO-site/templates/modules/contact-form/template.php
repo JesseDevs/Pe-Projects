5b0719f86ac38a5f002f4295892d9e8d1583613f
Jesse <jessemerge2@gmail.com>
2022-08-06 16:01:03 -0700

    add contact form module

---
+++ projects/PRO-site/templates/modules/contact-form/template.php
@@ -0,0 +1,22 @@
+<contact-form>
+    <h2>Contact Me</h2>
+    <p class="intro">Lets talk about code? Maybe some life discussion? Or some of the latest music out?</p>
+
+    <field>    
+        <label for="">NAME</label>
+        <input type="text">
+    </field>
+
+
+    <field>    
+        <label for="">EMAIL</label>
+        <input type="email">
+    </field>
+
+
+    <field class='message'>    
+        <label for="">MESSAGE</label>
+        <input type="text">
+    </field>
+
+</contact-form>
