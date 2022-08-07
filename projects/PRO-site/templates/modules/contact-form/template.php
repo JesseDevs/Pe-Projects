03d18e480d070a716389be09a9b2afc17bd732f4
Jesse <jessemerge2@gmail.com>
2022-08-07 16:37:40 -0700

    finish contact form

--- projects/PRO-site/templates/modules/contact-form/template.php
+++ projects/PRO-site/templates/modules/contact-form/template.php
@@ -1,22 +1,36 @@
-<contact-form>
-    <h2 class='yell-voice'>Contact Me </h2>
-    <p class="intro">Lets talk about code? Maybe some life discussion? Or some of the latest music out?</p>
 
-    <field>    
-        <label for="">NAME</label>
-        <input type="text">
-    </field>
+<section class='connect'>
+
+    <inner-column> 
+        <contact-form>
+            <h2 class='yell-voice'>Contact Me </h2>
+            <p class="intro">Lets talk about code? Maybe some life discussion? Or some of the latest music out?</p>
+
+            <form>   
+                <field>    
+                   <label for="">NAME</label>
+                   <input type="text" placeholder="Jesse">
+               </field>
 
 
-    <field>    
-        <label for="">EMAIL</label>
-        <input type="email">
-    </field>
+               <field>    
+                   <label for="">EMAIL</label>
+                   <input type="email">
+               </field>
 
 
-    <field class='message'>    
-        <label for="">MESSAGE</label>
-        <input type="text">
-    </field>
+               <field class='message'>    
+                   <label for="">MESSAGE</label>
+                   <input type="text" placeholder="Something to say..">
+               </field>
 
-</contact-form>
+
+               <button>CONNECT</button>
+           </form>
+
+
+
+       </contact-form>
+   </inner-column>
+
+</section>
