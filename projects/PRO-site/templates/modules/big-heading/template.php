12b9e2b5f7f4621ee4647e1a1a23e2a5c35a7c61
Jesse <jessemerge2@gmail.com>
2022-08-07 16:37:16 -0700

    adjust big heading template

--- projects/PRO-site/templates/modules/big-heading/template.php
+++ projects/PRO-site/templates/modules/big-heading/template.php
@@ -3,6 +3,10 @@ $heading = $section['heading'] ?? ucfirst($page);
 ?>
 
 <big-heading>
-    <h1 class="loud-voice"><?= $heading ?>!!</h1>
-    <img src=" <?= $section['image'] ?>"  alt="comp">
+    <h1 class="roar-voice"><?= $heading ?></h1>
+    <p class="intro"><?= $section['intro']?></p>
+    <?php if (isset($section['image'])){?>
+        <img src=" <?= $section['image'] ?>"  alt="thing">
+
+    <?php }?>
 </big-heading>
