34a392ab07f56165d6330ffb0be9398df2c80031
Jesse <jessemerge2@gmail.com>
2022-08-07 16:36:44 -0700

    add resume function filter

--- projects/PRO-site/functions/routing.php
+++ projects/PRO-site/functions/routing.php
@@ -16,6 +16,11 @@ if ($page == 'home') {
     $pageData = json_decode($json, true);
 }
 
+if ($page == 'about') {
+    $json = file_get_contents('data/pages/about.json');
+    $pageData = json_decode($json, true);
+}
+
 if ($page == 'projects') {
     $jsonPage = file_get_contents('data/pages/projects.json');
     $pageData = json_decode($jsonPage, true);
@@ -41,4 +46,31 @@ function currentPage(){
 
     }
 
+}
+
+function activePage($name)
+{
+    if ($_GET['page'] == $name) {
+        echo "active";
+    }
+}
+
+function filterArrayByType($array, $type)
+{
+    $filteredArray = [];
+    foreach ($array as $item) {
+        if ($item["type"] == $type) {
+            array_push($filteredArray, $item);
+        }
+    }
+    return $filteredArray;
+}
+
+
+function renderList($array, $type)
+{
+    $list = filterArrayByType($array, $type);
+    foreach ($list as $item) {
+        include("templates/modules/resume-card/template.php");
+    }
 }
