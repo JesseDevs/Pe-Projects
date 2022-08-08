4b5b3b1f028b6085d0700f1a343e6993ddc59edb
Jesse <jessemerge2@gmail.com>
2022-08-08 00:58:49 -0700

    add hamburger menu

--- projects/portfolio/templates/modules/site-footer/template.php
+++ projects/portfolio/templates/modules/site-footer/template.php
@@ -9,6 +9,19 @@
                 <li><a href="?page=writing " class="<?php activePage("writing") ?>">Writing</a></li>
             </ul>
 
+            <ul>
+                <li><a href="https://github.com/JesseDevs">GitHub</a></li>
+                <li><a href="https://twitter.com/Jesse_Devs">Twitter</a></li>
+                <li><a href="https://www.linkedin.com/in/felix-jesser1223/">LinkedIn</a></li>
+                <li><a href="https://www.instagram.com/jessedude2/">Instagram</a></li>
+                <li><a href="https://jessedevs.substack.com/">Blog</a></li>
+                
+            </ul>
+
+            <ul>
+                <li>© 2022 Portfolio from Jesse</li>
+            </ul>
+
         </nav>
 
     </inner-column>
--- projects/portfolio/templates/modules/site-menu/template.php
+++ projects/portfolio/templates/modules/site-menu/template.php
@@ -1,8 +1,9 @@
 <header class='site-header'>
     <inner-column>
 
-        <nav>
+        <nav class='main-menu'>
             <ul>
+
                 <li><a href="?page=home" class=" <?php activePage("home") ?>">Home</a></li>
                 <li><a href="?page=about" class=" <?php activePage("about") ?>">About</a></li>
                 <li><a href="?page=projects" class=" <?php activePage("projects") ?>">Projects</a></li>
@@ -11,6 +12,23 @@
 
         </nav>
 
-    </inner-column>
+        <nav class='burger'>
+            <input id="menu-toggle" type="checkbox" />
+            <label class='menu-button-container' for="menu-toggle">
+                <div class='menu-button'></div>
+            </label>
+            <nav class="menu">
+               <a class="small-size" href="?page=home" class=" <?php activePage("home") ?>">Home</a>
+               <a class="small-size" href="?page=about" class=" <?php activePage("about") ?>">About</a>
+               <a class="small-size" href="?page=projects" class=" <?php activePage("projects") ?>">Projects</a>
+               <a class="small-size" href="?page=writing " class="<?php activePage("writing") ?>">Writing</a>
+
+               <a href="?page=e4p" class="<?php activePage("e4p") ?>">Exercises For Programmers</a>
+               <a href="?page=garden" class="<?php activePage("garden") ?>">Layout Garden</a>
+           </nav>
+
+       </nav>
+
+   </inner-column>
 </header>
 
