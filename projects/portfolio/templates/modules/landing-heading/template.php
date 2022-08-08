77a6037ed21d623d6d6cd46a6d958aa491f47951
Jesse <jessemerge2@gmail.com>
2022-08-08 03:00:24 -0700

    start building grid

--- projects/portfolio/templates/modules/landing-heading/template.php
+++ projects/portfolio/templates/modules/landing-heading/template.php
@@ -4,16 +4,25 @@ $heading = $section['heading'] ?? ucfirst($page);
 
 <landing-heading>
     <h1 class="roar-voice"><?= $heading ?></h1>
-    <div class="intro"> <?= $section["intro"] ?></div>
+    <text-content>
+        <div class="intro"> <?= $section["intro"] ?></div>
+        
+        <p class="details"><?= $section["details"] ?></p>
+    </text-content>
 
     <picture class="landing-image">
-
-        <p class="details"><?= $section["details"] ?></p>
 
         <img src=" <?= $section['image'] ?>"  alt="comp">
     </picture>
 
+
+
     <svg class="arrows">
+
+        <path stroke-linecap="round" class="a1" d="M0 0 L20 22 L40 0"></path>
+    </svg>
+    <svg class="arrows arrows-two">
+
         <path stroke-linecap="round" class="a1" d="M0 0 L20 22 L40 0"></path>
     </svg>
 
--- projects/portfolio/templates/modules/project-card/template.php
+++ projects/portfolio/templates/modules/project-card/template.php
@@ -1,19 +1,21 @@
 <project-card>
-	<span class='one'><?= strtoupper($project['intro']) ?></span>
-	<h3 class='attention-voice'><?= $project['title'] ?></h3>
 
-	<text-content class="skills">
-		<?php foreach ($project['skills'] as $skill){ ?>
-			<p class="small-voice"><?= $skill ?></p>
-		<?php } ?>
-	</text-content>
+	<span><?= strtoupper($project['intro']) ?></span>
+	<div>
+		<h3 class='attention-voice'><?= $project['title'] ?></h3>
 
-	<a href="?page=project&id=<?=$project["id"] ?>">
+		<text-content class="skills">
+			<?php foreach ($project['skills'] as $skill){ ?>
+				<p class="small-voice"><?= $skill ?></p>
+			<?php } ?>
+		</text-content>
+	</div>
+	<a class='project-link' href="?page=project&id=<?=$project["id"] ?>">
 		<picture class='project-thumbnail'>
 			<img src=" <?= $project['thumbnail'] ?>" alt="">
 		</picture>
 	</a>
-	<p><?= $project['detail'] ?></p>
+	<p class="project-detail"><?= $project['detail'] ?></p>
 	<text-content class="project-actions">
 		<?php foreach ($project['links'] as $link){ ?>
 			<a href='<?= $link['link'] ?>'><?= $link['title'] ?></a>
