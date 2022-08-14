6cfa5c6435f14490e9178bdbe8a2b01709f2ef02
Jesse <jessemerge2@gmail.com>
2022-08-13 23:42:19 -0700

    change button size

--- templates/modules/contact-form/template.php
+++ templates/modules/contact-form/template.php
@@ -1,28 +1,29 @@
 
 <contact-form>
-    <h2 class='yell-voice'><?= $section['heading']?> </h2>
-    <p class="intro"><?= $section['intro']?></p>
-
-    <form>   
-        <field>    
-           <label for=""><?= $section['labelOne']?></label>
-           <input type="text" placeholder="<?= $section['holderOne']?>">
-     </field>
+  <h2 class='yell-voice'><?= $section['heading']?> </h2>
+  <p class="intro"><?= $section['intro']?></p>
 
 
-     <field>    
-       <label for=""><?= $section['labelTwo']?></label>
-         <input type="email">
-     </field>
+  <form>   
+    <field>    
+     <label for=""><?= $section['labelOne']?></label>
+     <input type="text" placeholder="<?= $section['holderOne']?>">
+   </field>
 
 
-     <field class='message'>    
-       <label for=""><?= $section['labelThree']?></label>
-       <input type="text" placeholder="<?= $section['holderTwo']?>">
-     </field>
+   <field>    
+     <label for=""><?= $section['labelTwo']?></label>
+     <input type="email">
+   </field>
 
 
-     <button><?= $section['button']?></button>
+   <field class='message'>    
+     <label for=""><?= $section['labelThree']?></label>
+     <input type="text" placeholder="<?= $section['holderTwo']?>">
+   </field>
+
+
+   <button><?= $section['button']?></button>
  </form>
 </contact-form>
 
