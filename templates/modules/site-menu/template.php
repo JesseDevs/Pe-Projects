384a0a285d475d1c2f955d9cc6ec1f3af6795824
Jesse <jessemerge2@gmail.com>
2022-08-15 22:22:57 -0700

    Add animation and separate svg files

--- templates/modules/site-menu/template.php
+++ templates/modules/site-menu/template.php
@@ -1,70 +1,49 @@
 <header class='site-header'>
     <inner-column>
         <ul>
-            <li class='svg-container'>
-                <svg width="100%" height="100%" viewBox="0 0 171 195" >
-                    <g class="cube" id="cube" transform="matrix(0.14969,0,0,0.231123,-74.785,-57.1995)">
-                        <rect x="499.598" y="247.485" width="1139.46" height="841.782"/>
-                        <g id="bottom" transform="matrix(6.68046,0,0,4.3267,-2281.08,-583.675)">
-                            <path d="M417.583,337.528L501.099,385.746L584.616,337.528L501.099,289.31L417.583,337.528Z"/>
-                        </g>
-                        <g id="back" transform="matrix(6.68046,0,0,4.3267,-2281.08,-583.675)">
-                            <path d="M584.616,241.092L501.099,192.873L501.099,289.31L584.616,337.528L584.616,241.092Z"/>
-                        </g>
-                        <g id="right" transform="matrix(6.68046,0,0,4.3267,-2281.08,-583.675)">
-                            <path d="M501.099,385.746L501.099,289.31L584.616,241.092L584.616,337.528L501.099,385.746Z"/>
-                        </g>
-                        <g id="left" transform="matrix(6.68046,0,0,4.3267,-2281.08,-583.675)">
-                            <path d="M501.099,192.873L417.583,241.092L417.583,337.528L501.099,289.31"/>
-                        </g>
-                        <g id="front" transform="matrix(6.68046,0,0,4.3267,-2281.08,-583.675)">
-                            <path d="M417.583,241.092L501.099,289.31L501.099,385.746L417.583,337.528L417.583,241.092Z"/>
-                        </g>
-                        <g id="top" transform="matrix(6.68046,0,0,4.3267,-2281.08,-583.675)">
-                            <path d="M417.583,241.092L501.099,192.873L584.616,241.092L501.099,289.31L417.583,241.092Z"/>
-                        </g>
-                    </g>
-                </svg>
-            </li>
-            <li><a href="?page=home">
-                <h1 class="logo">Jesse&nbsp;Devs</h1>
-            </a>
+         <?php include("templates/svg.php");
+         ?>
+     </li>
+     <li>
+        <a href="?page=home">
+            <h1 class="logo">Jesse&nbsp;Devs</h1>
+        </a>
+    </li>
+</ul>
+
+
+
+<nav class='main-menu'>
+    <ul>
+
+        <li><a href="?page=home" class=" <?php activePage("home") ?>">Home</a></li>
+        <li><a href="?page=about" class=" <?php activePage("about") ?>">About</a></li>
+        <li><a href="?page=projects" class=" <?php activePage("projects") ?>">Projects</a></li>
+        <li class="large-size">
+            <a href="?page=writing " class="<?php activePage("writing") ?>">Writing</a>
+        </li>
+        <li class="larger-size">
+            <a href="?page=forms " class="<?php activePage("forms") ?>">Forms</a>
         </li>
     </ul>
 
+</nav>
 
+<nav class='burger'>
+    <input id="menu-toggle" type="checkbox" />
+    <label class='menu-button-container' for="menu-toggle">
+        <div class='menu-button'></div>
+    </label>
+    <nav class="menu">
+       <a class="small-size" href="?page=home" class=" <?php activePage("home") ?>">Home</a>
+       <a class="small-size" href="?page=about" class=" <?php activePage("about") ?>">About</a>
+       <a class="small-size" href="?page=projects" class=" <?php activePage("projects") ?>">Projects</a>
+       <a href="?page=writing " class="<?php activePage("writing") ?>">Writing</a>
 
-    <nav class='main-menu'>
-        <ul>
-
-            <li><a href="?page=home" class=" <?php activePage("home") ?>">Home</a></li>
-            <li><a href="?page=about" class=" <?php activePage("about") ?>">About</a></li>
-            <li><a href="?page=projects" class=" <?php activePage("projects") ?>">Projects</a></li>
-            <li class="large-size">
-                <a href="?page=writing " class="<?php activePage("writing") ?>">Writing</a>
-            </li>
-            <li class="larger-size">
-                <a href="?page=forms " class="<?php activePage("forms") ?>">Forms</a>
-            </li>
-        </ul>
-
-    </nav>
-
-    <nav class='burger'>
-        <input id="menu-toggle" type="checkbox" />
-        <label class='menu-button-container' for="menu-toggle">
-            <div class='menu-button'></div>
-        </label>
-        <nav class="menu">
-           <a class="small-size" href="?page=home" class=" <?php activePage("home") ?>">Home</a>
-           <a class="small-size" href="?page=about" class=" <?php activePage("about") ?>">About</a>
-           <a class="small-size" href="?page=projects" class=" <?php activePage("projects") ?>">Projects</a>
-           <a href="?page=writing " class="<?php activePage("writing") ?>">Writing</a>
-
-           <a href="?page=forms" class="<?php activePage("forms") ?>">Exercises</a>
-       </nav>
-
+       <a href="?page=forms" class="<?php activePage("forms") ?>">Exercises</a>
    </nav>
+
+</nav>
 
 
 </inner-column>
