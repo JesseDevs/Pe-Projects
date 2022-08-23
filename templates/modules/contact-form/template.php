6012cc9d09c6654d991898c5497090ea8a5cf6fc
Jesse <jessemerge2@gmail.com>
2022-08-23 01:54:19 -0700

    Get contact form to deliver message. TEST

--- templates/modules/contact-form/template.php
+++ templates/modules/contact-form/template.php
@@ -1,39 +1,47 @@
+<?php if (isset($_POST['submit'])) {
+  $to = "jessemerge2@gmail.com";
+  $subject = "Message from Website";
+  $message = $_POST['message'];
+  $from = $_POST['email'];
+  $headers = "From:" . $from;
+  mail($to, $subject, $message, $headers);
+  echo "Your message has been sent.";
+} ?>
 
 <contact-form id="contact">
-  <h2 class='yell-voice'><?= $section['heading']?> </h2>
-  <p class="intro"><?= $section['intro']?></p>
+  <h2 class='yell-voice'><?= $section['heading'] ?> </h2>
+  <p class="intro"><?= $section['intro'] ?></p>
 
 
-  <form>   
-    <field>    
-     <label for=""><?= $section['labelOne']?></label>
-     <input type="text" placeholder="<?= $section['holderOne']?>">
-   </field>
+  <form method="POST">
+    <field>
+      <label for=""><?= $section['labelOne'] ?></label>
+      <input type="text" placeholder="<?= $section['holderOne'] ?>">
+    </field>
 
 
-   <field>    
-     <label for=""><?= $section['labelTwo']?></label>
-     <input type="email">
-   </field>
+    <field>
+      <label for=""><?= $section['labelTwo'] ?></label>
+      <input name='email' type="email">
+    </field>
 
 
-   <field class='message'>    
-     <label for=""><?= $section['labelThree']?></label>
-     <textarea type="text" placeholder="<?= $section['holderTwo']?>"></textarea> 
-   </field>
+    <field class='message'>
+      <label for=""><?= $section['labelThree'] ?></label>
+      <textarea name="message" type="text" placeholder="<?= $section['holderTwo'] ?>"></textarea>
+    </field>
 
 
-   <button><?= $section['button']?></button>
- </form>
+    <button type="submit" name="submit" value="Send Message"><?= $section['button'] ?></button>
+  </form>
 
 
 
- 
 
- <div class='fade'>
-   <div class="cube-sphere"> 
-    <?php include('templates/cube-sphere.php');?>
+
+  <div class='fade'>
+    <div class="cube-sphere">
+      <?php include('templates/cube-sphere.php'); ?>
+    </div>
   </div>
-</div>
 </contact-form>
-
