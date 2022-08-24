c31d12740b71cc8179440c861bfd91cd346534a4
Jesse <jessemerge2@gmail.com>
2022-08-23 19:03:49 -0700

    Text content changes and containers

--- templates/modules/landing-heading/template.php
+++ templates/modules/landing-heading/template.php
@@ -3,28 +3,17 @@ $heading = $section['heading'] ?? ucfirst($page);
 ?>
 
 <landing-heading>
-    <h1 class="roar-voice"><?= $heading ?></h1>
     <text-content>
-        <div class="intro"> <?= $section["intro"] ?></div>
-        
+        <h1 class="roar-voice"><?= $heading ?></h1>
+
+        <p class='intro'> <?= $section["intro"] ?></p>
+
         <p class="details"><?= $section["details"] ?></p>
     </text-content>
 
     <picture class="landing-image">
 
-        <img src=" <?= $section['image'] ?>"  alt="comp">
+        <img src=" <?= $section['image'] ?>" alt="comp">
     </picture>
-
-
-
-  <!--   <svg class="arrows">
-
-        <path stroke-linecap="round" class="a1" d="M0 0 L20 22 L40 0"></path>
-    </svg>
-    <svg class="arrows arrows-two">
-
-        <path stroke-linecap="round" class="a1" d="M0 0 L20 22 L40 0"></path>
-    </svg> -->
-
 
 </landing-heading>
