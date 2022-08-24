0d122cd1e727be21682b6dfa15191dba64b7278d
Jesse <jessemerge2@gmail.com>
2022-08-24 10:01:51 -0700

    404 update

--- templates/modules/missing-heading/template.php
+++ templates/modules/missing-heading/template.php
@@ -5,20 +5,20 @@ $heading = $section['heading'] ?? ucfirst($page);
 <missing-heading>
     <text-content>
         <h1 class="roar-voice"><?= $heading ?></h1>
-        <p class="intro"><?= $section['intro']?></p>
+        <p class="intro"><?= $section['intro'] ?></p>
     </text-content>
 
     <nav>
         <ul>
-            <?php foreach ($section['links'] as $link){ ?>
+            <?php foreach ($section['links'] as $link) { ?>
 
-                <li><a href="<?= $link['link']?>"><?= $link['title']?></a></li>
+                <li><a target="_blank" href="<?= $link['link'] ?>"><?= $link['title'] ?></a></li>
             <?php } ?>
         </ul>
     </nav>
 
     <div class="cube-sphere">
-        <?php include('templates/cube-sphere.php');?>
+        <?php include('templates/cube-sphere.php'); ?>
     </div>
 
 </missing-heading>
