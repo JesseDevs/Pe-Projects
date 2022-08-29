c52a9123a531e4e8f57f048a7763679c62f07f95
Jesse <jessemerge2@gmail.com>
2022-08-28 19:56:39 -0700

    Add hamburger menu with JS

---
+++ projects/manga/file.js
@@ -0,0 +1,11 @@
+var body = document.querySelector('body');
+var toggle = document.querySelector('button');
+
+// hamburger menu
+body.addEventListener('click', function (event) {
+    //if the click happens on the toggle button
+
+    if (event.target.matches('[rel="toggle"]')) {
+        body.classList.toggle('menu-open');
+    }
+})
--- projects/manga/templates/modules/site-menu/template.php
+++ projects/manga/templates/modules/site-menu/template.php
@@ -1,6 +1,21 @@
 <header class='site-header'>
     <inner-column>
-        <nav>
+        <nav class='main-menu'>
+            <ul>
+                <li><a class='logo' href="?page=home">The Reads</a></li>
+                <li>
+
+                    <button rel="toggle">Menu Toggle</button>
+                    <nav class="burger">
+                        <a href="#">Thing one</a>
+                        <a href="#">Thing two</a>
+                        <a href="#">Thing three</a>
+                        <button rel="toggle">Close Toggle</button>
+                    </nav>
+                </li>
+            </ul>
+        </nav>
+        <nav class='side-bar'>
             <a class='logo' href="?page=home">The Reads</a>
             <ul>
                 <li><a href="?page=home">Home</a></li>
