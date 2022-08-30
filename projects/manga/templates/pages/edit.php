3d3bbd67aea7d8db9119c0ee65995953f4950498
Jesse <jessemerge2@gmail.com>
2022-08-29 21:00:49 -0700

    modules not pages in include function

--- projects/manga/templates/pages/edit.php
+++ projects/manga/templates/pages/edit.php
@@ -17,7 +17,7 @@ $mangas = getMangas()["mangas"];
 <section class="display-manga">
     <inner-column>
         <?php
-        include("templates/pages/detail-card/template.php");
+        include("templates/modules/detail-card/template.php");
         ?>
     </inner-column>
 </section>
