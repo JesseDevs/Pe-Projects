ec898f186d19ec5c3ec2bfb82e1a4467415da980
Jesse <jessemerge2@gmail.com>
2022-08-30 02:28:10 -0700

    Add media queries

--- projects/manga/templates/modules/landing/template.php
+++ projects/manga/templates/modules/landing/template.php
@@ -2,10 +2,12 @@
     <text-content>
 
         <span class='attention-voice'>Welcome <em class='strict-voice'> to</em></span>
-        <h1 class='roar-voice'>
-            The Reads
-        </h1>
-        <p class='landing-intro'>Let's get lost</p>
+        <div class="title">
+            <h1 class='roar-voice'>
+                The Reads
+            </h1>
+            <p class='landing-intro'>Let's get lost</p>
+        </div>
     </text-content>
 
 </landing-block>
--- projects/manga/templates/modules/site-menu/template.php
+++ projects/manga/templates/modules/site-menu/template.php
@@ -21,13 +21,10 @@
             </ul>
         </nav>
         <nav class='side-bar'>
-            <a class='logo' href="?page=home">The Reads</a>
             <ul>
                 <li><a href="?page=home">Home</a></li>
                 <li><a href="?page=list">Catalog</a></li>
-                <li>
-                    <span>?<?= printQueryString(); ?></span>
-                </li>
+
             </ul>
 
         </nav>
