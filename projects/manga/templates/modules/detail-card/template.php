0bbac40cca06623853ea54c3cc5ebeda486308bb
Jesse <jessemerge2@gmail.com>
2022-08-29 20:48:40 -0700

    Detail card

---
+++ projects/manga/templates/modules/detail-card/template.php
@@ -0,0 +1,13 @@
+<detail-card>
+    <text-content>
+
+        <h2 class="roar-voice"> <?= $currentManga['title'] ?></h2>
+        <p>The author of this book / manga is <strong><?= ucwords($currentManga['author']) ?></strong></p>
+        <p>This book / manga has <strong><?= $currentManga['chapters'] ?>!</strong></p>
+    </text-content>
+
+    <div class="detail-actions">
+        <a class=" button delete" href="?page=delete&id=<?= $currentManga['id'] ?>"><span>Delete</span></a>
+        <a class=" button edit" href="?page=edit&id=<?= $currentManga['id'] ?>"><span>Edit</span></a>
+    </div>
+</detail-card>
