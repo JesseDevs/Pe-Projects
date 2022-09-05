1c56b170185f1f574f01e96de53b72817f84279d
Jesse <jessemerge2@gmail.com>
2022-09-04 21:47:21 -0700

    Create modules

--- templates/case/diptech/template.php
+++ templates/case/diptech/template.php
@@ -6,16 +6,30 @@ if ($article["flipped"]) {
     $flipped = "flipped";
 }
 
+$article["title"]
+
 ?>
 
+
+
 <diptech class=' <?= $flipped ?>'>
+
+    <text-content>
+
+        <?php if ($article["title"]) { ?>
+            <h3 class="attention-voice">
+                <?= $article["title"] ?>
+            </h3>
+        <?php } ?>
+
+        <p>
+            <?= $article["text"] ?>
+        </p>
+    </text-content>
 
     <picture>
         <img src="<?= $article["image"] ?>" alt="">
     </picture>
 
-    <p>
-        <?= $article["text"] ?>
-    </p>
 
 </diptech>
