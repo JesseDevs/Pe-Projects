82e426e7702053ac3338cb7f71e5980aa0d1af09
Jesse <jessemerge2@gmail.com>
2022-09-05 14:23:04 -0700

    Create template and modules for template 1

---
+++ projects/wireframe/templates/components/subscribe-block/template.php
@@ -0,0 +1,8 @@
+<subscribe-block>
+    <p class'list-header'>Subscribe</p>
+    <input type="email" placeholder="Email address">
+    <button> &#62; </button>
+    <p class='small-voice'>
+        Gravida sed justo, justo, id est et. Amet tristique convallis sed porttitor nullam eu ut. Duis et odio aliquam bibendum. Metus et lectus id viverra fringilla magna morbi.
+    </p>
+</subscribe-block>
---
+++ projects/wireframe/templates/modules/landing-block/template.php
@@ -0,0 +1,18 @@
+<?php
+$heading = $section['heading'] ?? ucfirst($page);
+?>
+
+<landing-block>
+    <text-content>
+        <h1 class="roar-voice"><?= $heading ?></h1>
+
+        <p class='intro'> <?= $section["intro"] ?></p>
+    </text-content>
+
+    <action-links>
+        <?php foreach ($section['actions'] as $link) { ?>
+            <a href="#"> <?= $link ?> </a>
+        <?php } ?>
+    </action-links>
+
+</landing-block>
