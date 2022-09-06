81ddec0074b138df14d21f327b4e50d8d2f810d0
Jesse <jessemerge2@gmail.com>
2022-09-06 02:36:31 -0700

    Create article card for article grid

---
+++ projects/wireframe/templates/components/article-card/template.php
@@ -0,0 +1,12 @@
+<article>
+    <picture>
+        <img src="<?= $article['img'] ?>" alt="">
+    </picture>
+
+    <text-content>
+
+        <h4 class="attention-voice"><?= $article['title'] ?></h4>
+        <p class="small-voice"><?= $article['detail'] ?></p>
+    </text-content>
+
+</article>
