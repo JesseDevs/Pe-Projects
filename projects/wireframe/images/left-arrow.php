73ca3fc3b2300461a2b69b8233a73d92cb37aa9b
Jesse <jessemerge2@gmail.com>
2022-09-07 01:33:30 -0700

    Work on question block and add new arrows

--- projects/wireframe/images/left-arrow.php
+++ projects/wireframe/images/left-arrow.php
@@ -1,3 +1,3 @@
-<svg width="16" height="27" viewBox="0 0 16 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M14.25 26L1.75 13.5L14.25 1" stroke="#212121" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+<svg width="50" height="51" viewBox="0 0 50 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M31.25 38L18.75 25.5L31.25 13" stroke="#212121" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
 </svg>
--- projects/wireframe/images/right-arrow.php
+++ projects/wireframe/images/right-arrow.php
@@ -1,3 +1,3 @@
-<svg width="16" height="27" viewBox="0 0 16 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M1.75 26L14.25 13.5L1.75 1" stroke="#212121" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+<svg width="50" height="51" viewBox="0 0 50 51" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M18.75 38L31.25 25.5L18.75 13" stroke="#212121" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
 </svg>
