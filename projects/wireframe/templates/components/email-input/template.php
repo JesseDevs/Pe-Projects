2e6a1b13ed6861e063e07ad20df0ae868f799356
Jesse <jessemerge2@gmail.com>
2022-09-07 19:46:38 -0700

    Create email input for subscribe box

---
+++ projects/wireframe/templates/components/email-input/template.php
@@ -0,0 +1,9 @@
+<email-input>
+
+    <input type="email" placeholder="Email address">
+
+    <a href="#" class='arrow'>
+
+        <?php include('images/button-arrow.php') ?>
+    </a>
+</email-input>
--- projects/wireframe/templates/components/subscribe-block/template.php
+++ projects/wireframe/templates/components/subscribe-block/template.php
@@ -1,8 +1,9 @@
 <subscribe-block>
-    <p class='list-header'>Subscribe</p>
-    <input type="email" placeholder="Email address">
-    <button> &#62; </button>
-    <p class='small-voice'>
+    <h5 class='small-voice '>Subscribe</h5>
+
+    <?php include('templates/components/email-input/template.php'); ?>
+
+    <p>
         Gravida sed justo, justo, id est et. Amet tristique convallis sed porttitor nullam eu ut. Duis et odio aliquam bibendum. Metus et lectus id viverra fringilla magna morbi.
     </p>
 </subscribe-block>
