5ef5db5154206e2fa122b36ac38ac768c99112b1
Jesse <jessemerge2@gmail.com>
2022-09-08 21:33:27 -0700

    Clip art background for template 2

--- projects/wireframe/templates/modules/landing-block/template.php
+++ projects/wireframe/templates/modules/landing-block/template.php
@@ -35,3 +35,18 @@ $heading = $section['heading'] ?? ucfirst($page);
 
 
 </landing-block>
+
+<divider>
+
+
+    <svg width="100%" height="100%" viewBox="0 0 1684 287" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+        <g transform="matrix(1,0,0,1,7.02556,-786.051)">
+            <g transform="matrix(1,0,0,1,-615.876,-749.808)">
+                <g transform="matrix(1.00695,0,0,1.02813,601.558,727.693)">
+                    <path d="M7.242,1064.26C7.242,1064.26 452.47,849.308 769.411,932.408C1086.35,1015.51 1042.45,1087.62 1529.24,861.339L1678.69,786.051L1678.69,1064.26L7.242,1064.26Z" style="fill:#FFFFFF" />
+                </g>
+            </g>
+        </g>
+    </svg>
+
+</divider>
