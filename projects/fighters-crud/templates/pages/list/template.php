30c4a2ed173ae17be5809ff27a4d31d0e464c364
Jesse <jessemerge2@gmail.com>
2022-09-29 02:18:30 -0700

    Fix up list page

--- projects/fighters-crud/templates/pages/list/template.php
+++ projects/fighters-crud/templates/pages/list/template.php
@@ -2,8 +2,7 @@
 
 $fighters = getFighters();
 
-
-showErrors();
+$playstyle = null;
 
 if (isset($_GET['playstyle'])) {
     $playstyle = $_GET['playstyle'];
@@ -11,7 +10,7 @@ if (isset($_GET['playstyle'])) {
 
 ?>
 
-<header class='list-intro'>
+<header class='playstyle-menu'>
 
     <h1 class='loud-voice'>CHOOSE YOUR FIGHTER</h1>
 
@@ -19,36 +18,23 @@ if (isset($_GET['playstyle'])) {
     <nav class='playstyle'>
         <a href="?page=list&playstyle=Shoto" class="<?= activeFighters('Shoto') ?>">Shoto</a>
 
-        <div class="dash">|</div>
         <a href="?page=list&playstyle=Charge" class="<?= activeFighters('Charge') ?>">Charge</a>
 
-        <div class="dash">|</div>
         <a href="?page=list&playstyle=Grappler" class="<?= activeFighters('Grappler') ?>">Grappler</a>
 
-        <div class="dash">|</div>
         <a href="?page=list&playstyle=Zoner" class="<?= activeFighters('Zoner') ?>">Zoner</a>
 
-        <div class="dash">|</div>
         <a href="?page=list" class="<?= activeFighters(null) ?>">Everyone</a>
     </nav>
+
 </header>
 
-<section class=" street-fighter list-grid">
-    <inner-column class='tight'>
+<section class="list-grid">
+    <inner-column>
         <ul class='fighters-list'>
             <?php foreach ($fighters as $fighter) { ?>
                 <li class='fighter <?= isSelected($playstyle, $fighter['playstyle']) ?>'>
-                    <a href="?page=detail&id=<?= $fighter['id'] ?>">
-                        <fighter-card>
-                            <h2>
-                                <?= $fighter['name'] ?>
-                            </h2>
-                            <?php if (isset($fighter['portrait'])) { ?>
-                                <picture style="background-image:url(<?= $fighter['portrait'] ?>)">
-                                </picture>
-                            <?php } ?>
-                        </fighter-card>
-                    </a>
+                    <?php include('templates/modules/fighter-card/template.php') ?>
                 </li>
             <?php } ?>
         </ul>
