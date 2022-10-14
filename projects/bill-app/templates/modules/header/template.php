c711568f99618532ebc20bddba3066db84cb4218
Jesse <jessemerge2@gmail.com>
2022-10-14 01:02:19 -0700

    Cute title animation added

--- projects/bill-app/templates/modules/header/template.php
+++ projects/bill-app/templates/modules/header/template.php
@@ -2,7 +2,7 @@
     <inner-column>
 
         <h1 class='yell-voice' data-route="home">
-            Bill / Split
+            Bill <span>/</span> <span>Split</span>
         </h1>
 
     </inner-column>
