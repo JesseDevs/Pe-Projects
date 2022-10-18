8c25465c5c9dcdf1ea9d64e09e527828e7c66623
Jesse <jessemerge2@gmail.com>
2022-10-18 10:55:52 -0700

    Small case study adjustments for reading purposees

--- templates/case/double-text/template.php
+++ templates/case/double-text/template.php
@@ -1,4 +1,9 @@
 <double-text>
+    <?php if (isset($article["title"])) { ?>
+        <h3 class="attention-voice">
+            <?= $article["title"] ?>
+        </h3>
+    <?php } ?>
     <p>
         <?= $article["paragraphOne"] ?>
     </p>
