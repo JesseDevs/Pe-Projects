9b1ba244281b8e113d85bd3ee8585efbc62cfc45
Jesse <jessemerge2@gmail.com>
2022-11-09 22:56:52 -0800

    Main personal site backend clean up page function

--- router/routing.php
+++ router/routing.php
@@ -1,90 +1,70 @@
 <?php
-
-$page = 'home';
-$pageData = null;
-
 if (isset($_GET['page'])) {
     $page = $_GET['page'];
 } else {
     $page = "home";
 }
 
-if ($page == 'home') {
-    $json = file_get_contents('data/pages/home.json');
-    $pageData = json_decode($json, true);
+function renderPage()
+{
+    if (isset($_GET['page'])) {
+        $page = $_GET['page'];
+    } else {
+        $page = "home";
+    }
 
+    if (file_exists("data/pages/" . $page . ".json")) {
+        // Great!
+    } else {
+        $page = '404';
+    }
+
+    $json = file_get_contents("data/pages/" . $page . ".json");
+    $pageData = json_decode($json, true);
     $projectsData = getProjectFile();
-}
 
-if ($page == 'about') {
-    $json = file_get_contents('data/pages/about.json');
-    $pageData = json_decode($json, true);
-}
+    if ($page == 'project') {
 
-if ($page == 'projects') {
-    $jsonPage = file_get_contents('data/pages/projects.json');
-    $pageData = json_decode($jsonPage, true);
-    $json = file_get_contents('data/projects.json');
-    $projectsData = json_decode($json, true);
-}
+        foreach ($projectsData as $project) {
+            if ($project['id'] == $_GET['id']) {
+                $projectData = $project;
+            }
+        }
 
-if ($page == 'project') {
-    $json = file_get_contents('data/projects.json');
-    $projectsData = json_decode($json, true);
-    foreach ($projectsData as $project) {
-        if ($project['id'] == $_GET['id']) {
-            $projectData = $project;
+        $metaImage = $project['thumbnail'];
+    }
+
+    if ($page == 'form') {
+        $json = file_get_contents('data/form.json');
+        $formFile = json_decode($json, true);
+        foreach ($formFile as $form) {
+            if ($form['id'] == $_GET['id']) {
+                $formThing = $form;
+            }
         }
     }
 
-    $jsonPage = file_get_contents('data/pages/project.json');
-    $pageData = json_decode($jsonPage, true);
-}
 
+    if ($page == 'style') {
 
-if ($page == 'writing') {
-    $json = file_get_contents('data/pages/writing.json');
-    $pageData = json_decode($json, true);
-}
+        $resume = file_get_contents('data/filler-resume.json');
+        $resumeData = json_decode($resume, true);
 
-if ($page == 'forms') {
-    $json = file_get_contents('data/pages/forms.json');
-    $pageData = json_decode($json, true);
-}
+        $goals = file_get_contents('data/filler-goals.json');
+        $goalsData = json_decode($goals, true);
 
-if ($page == 'form') {
-    $json = file_get_contents('data/form.json');
-    $formFile = json_decode($json, true);
-    foreach ($formFile as $form) {
-        if ($form['id'] == $_GET['id']) {
-            $formThing = $form;
-        }
+        $json = file_get_contents('data/form.json');
+        $formData = json_decode($json, true);
+
+        $projects = file_get_contents('data/filler-projects.json');
+        $projectsData = json_decode($projects, true);
+
+        $projectData = $projectsData[0];
     }
 
-    $json = file_get_contents('data/pages/form.json');
-    $pageData = json_decode($json, true);
+    include('templates/pages/standard.php');
 }
 
-
-if ($page == 'style') {
-    $json = file_get_contents('data/pages/style.json');
-    $pageData = json_decode($json, true);
-
-
-    $resume = file_get_contents('data/filler-resume.json');
-    $resumeData = json_decode($resume, true);
-
-    $goals = file_get_contents('data/filler-goals.json');
-    $goalsData = json_decode($goals, true);
-
-    $json = file_get_contents('data/form.json');
-    $formData = json_decode($json, true);
-
-    $projects = file_get_contents('data/filler-projects.json');
-    $projectsData = json_decode($projects, true);
-
-    $projectData = $projectsData[0];
-}
 
 
 
