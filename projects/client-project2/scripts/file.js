e4787eb1b2c560b13bf65bceb2a74fe654382de4
Jesse <jessemerge2@gmail.com>
2022-11-17 00:13:50 -0800

    Get hamburger set up

--- projects/client-project2/scripts/file.js
+++ projects/client-project2/scripts/file.js
@@ -1,18 +1,4 @@
-// window size add class --- - -
-// window.addEventListener('resize', function () {
-//     if (window.innerWidth > 2000) {
-//     }
-// });
-
 var header = document.querySelector("header");
-const numberFormatter = Intl.NumberFormat('en-US');
-
-// converting first letter to uppercase
-function capitalizeFirstLetter(str) {
-    const capitalized = str.charAt(0).toUpperCase() + str.slice(1);
-
-    return capitalized;
-}
 
 window.addEventListener('click', function (event) {
 
--- projects/client-project2/templates/modules/site-menu/template.php
+++ projects/client-project2/templates/modules/site-menu/template.php
@@ -3,11 +3,12 @@
 $menu = file_get_contents("data/menu.json");
 $menuData = json_decode($menu, true);
 
+$menuButton = $menuData['button'];
 ?>
 
 <header class='site-header'>
     <inner-column>
-        <nav>
+        <nav class="main-menu">
             <ul>
                 <?php foreach ($menuData['actions'] as $action) { ?>
                     <li>
@@ -17,9 +18,25 @@ $menuData = json_decode($menu, true);
             </ul>
         </nav>
 
-        <?php $menuButton = $menuData['button']; ?>
+        <button class='hamburger-button' id='toggle' rel='hamburger'>
+            <div class="bar-top"></div>
+            <div class="bar-middle"></div>
+            <div class="bar-bottom"></div>
+        </button>
+
+        <nav class="burger">
+            <ul>
+                <?php foreach ($menuData['actions'] as $action) { ?>
+                    <li>
+                        <a href="<?= $action["link"] ?>" target="_blank" rel=""><span><?= $action["link-name"] ?></span></a>
+                    </li>
+                <?php } ?>
+            </ul>
+        </nav>
+
         <a class='header-button' href="<?= $menuButton["link"] ?>" target="_blank" rel="">
-            <span><?= $menuButton["link-name"] ?></span></a>
+            <span><?= $menuButton["link-name"] ?></span>
+        </a>
 
 
     </inner-column>
