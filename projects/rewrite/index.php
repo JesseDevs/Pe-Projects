f426a4d12df5e8048e09abf4f6186436d5bd5a15
Jesse <jessemerge2@gmail.com>
2023-01-13 14:51:20 -0800

    rewrite url project

---
+++ projects/rewrite/index.php
@@ -0,0 +1,13 @@
+<?php include('header.php'); ?>
+
+<?php renderTemplate(); ?>
+
+<?php
+	echo dirname($_SERVER['SCRIPT_NAME']);
+?>
+<br>
+
+<?=dirname($_SERVER['SCRIPT_NAME'])?>
+
+
+<?php include('footer.php'); ?>
---
+++ projects/rewrite/templates/components/site-menu.php
@@ -0,0 +1,22 @@
+
+<nav class='site-menu'>
+	<ul>
+		<li>
+			<a href='/'>Home</a>
+		</li>
+
+		<li>
+			<a href='about'>About</a>
+		</li>
+
+		<li>
+			<a href='contact'>Contact</a>
+		</li>
+
+		<li>
+			<a href='projects'>Projects</a>
+		</li>
+	</ul>
+
+	<code><?=queryString()?></code>
+</nav>
