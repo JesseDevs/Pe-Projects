2e00f01cb7d33fd85d536af28e13911ee6a72cdb
Jesse <jessemerge2@gmail.com>
2023-01-13 14:51:37 -0800

    htaccess test

--- router/routing.php
+++ router/routing.php
@@ -1,68 +1,68 @@
 <?php
 if (isset($_GET['page'])) {
-    $page = $_GET['page'];
+	$page = $_GET['page'];
 } else {
-    $page = "home";
+	$page = "home";
 }
 
 function renderPage()
 {
-    if (isset($_GET['page'])) {
-        $page = $_GET['page'];
-    } else {
-        $page = "home";
-    }
+	if (isset($_GET['page'])) {
+		$page = $_GET['page'];
+	} else {
+		$page = "home";
+	}
 
-    if (file_exists("data/pages/" . $page . ".json")) {
-        // Great!
-    } else {
-        $page = '404';
-    }
+	if (file_exists("data/pages/" . $page . ".json")) {
+		// Great!
+	} else {
+		$page = '404';
+	}
 
-    $json = file_get_contents("data/pages/" . $page . ".json");
-    $pageData = json_decode($json, true);
-    $projectsData = getProjectFile();
+	$json = file_get_contents("data/pages/" . $page . ".json");
+	$pageData = json_decode($json, true);
+	$projectsData = getProjectFile();
 
-    if ($page == 'project') {
+	if ($page == 'project') {
 
-        foreach ($projectsData as $project) {
-            if ($project['id'] == $_GET['id']) {
-                $projectData = $project;
-            }
-        }
+		foreach ($projectsData as $project) {
+			if ($project['id'] == $_GET['id']) {
+				$projectData = $project;
+			}
+		}
 
-        $metaImage = $project['thumbnail'];
-    }
+		$metaImage = $project['thumbnail'];
+	}
 
-    if ($page == 'form') {
-        $json = file_get_contents('data/form.json');
-        $formFile = json_decode($json, true);
-        foreach ($formFile as $form) {
-            if ($form['id'] == $_GET['id']) {
-                $formThing = $form;
-            }
-        }
-    }
+	if ($page == 'form') {
+		$json = file_get_contents('data/form.json');
+		$formFile = json_decode($json, true);
+		foreach ($formFile as $form) {
+			if ($form['id'] == $_GET['id']) {
+				$formThing = $form;
+			}
+		}
+	}
 
 
-    if ($page == 'style') {
+	if ($page == 'style') {
 
-        $resume = file_get_contents('data/filler-resume.json');
-        $resumeData = json_decode($resume, true);
+		$resume = file_get_contents('data/filler-resume.json');
+		$resumeData = json_decode($resume, true);
 
-        $goals = file_get_contents('data/filler-goals.json');
-        $goalsData = json_decode($goals, true);
+		$goals = file_get_contents('data/filler-goals.json');
+		$goalsData = json_decode($goals, true);
 
-        $json = file_get_contents('data/form.json');
-        $formData = json_decode($json, true);
+		$json = file_get_contents('data/form.json');
+		$formData = json_decode($json, true);
 
-        $projects = file_get_contents('data/filler-projects.json');
-        $projectsData = json_decode($projects, true);
+		$projects = file_get_contents('data/filler-projects.json');
+		$projectsData = json_decode($projects, true);
 
-        $projectData = $projectsData[0];
-    }
+		$projectData = $projectsData[0];
+	}
 
-    include('templates/pages/standard.php');
+	include('templates/pages/standard.php');
 }
 
 
@@ -70,38 +70,38 @@ function renderPage()
 
 function currentPage()
 {
-    if (isset($_GET['page'])) {
-        $page = $_GET['page'];
-    } else {
-        $page = "home";
-    }
+	if (isset($_GET['page'])) {
+		$page = $_GET['page'];
+	} else {
+		$page = "home";
+	}
 }
 
 function activePage($name)
 {
-    if (isset($_GET['page'])) {
-        if ($_GET['page'] == $name) {
-            echo "active";
-        }
-    }
+	if (isset($_GET['page'])) {
+		if ($_GET['page'] == $name) {
+			echo "active";
+		}
+	}
 }
 
 function filterArrayByType($array, $type)
 {
-    $filteredArray = [];
-    foreach ($array as $item) {
-        if ($item["type"] == $type) {
-            array_push($filteredArray, $item);
-        }
-    }
-    return $filteredArray;
+	$filteredArray = [];
+	foreach ($array as $item) {
+		if ($item["type"] == $type) {
+			array_push($filteredArray, $item);
+		}
+	}
+	return $filteredArray;
 }
 
 
 function renderList($array, $type)
 {
-    $list = filterArrayByType($array, $type);
-    foreach ($list as $item) {
-        include("templates/modules/resume-card/template.php");
-    }
+	$list = filterArrayByType($array, $type);
+	foreach ($list as $item) {
+		include("templates/modules/resume-card/template.php");
+	}
 }
--- templates/modules/site-menu/template.php
+++ templates/modules/site-menu/template.php
@@ -1,62 +1,62 @@
 <header class='main-tool-bar site-header'>
-    <inner-column>
-        <ul class='logo-container'>
-            <li class="svg-box">
-                <div class="container">
-                    <div class="cube">
-                        <div class="face top"></div>
-                        <div class="face bottom"></div>
-                        <div class="face left"></div>
-                        <div class="face right"></div>
-                        <div class="face front"></div>
-                        <div class="face back"></div>
-                    </div>
-                </div>
-            </li>
-            <li>
-                <a class='logo-link' href="?page=home">
-                    <h1 class="logo">Jesse&nbsp;Devs</h1>
+	<inner-column>
+		<ul class='logo-container'>
+			<li class="svg-box">
+				<div class="container">
+					<div class="cube">
+						<div class="face top"></div>
+						<div class="face bottom"></div>
+						<div class="face left"></div>
+						<div class="face right"></div>
+						<div class="face front"></div>
+						<div class="face back"></div>
+					</div>
+				</div>
+			</li>
+			<li>
+				<a class='logo-link' href="?page=home">
+					<h1 class="logo">Jesse&nbsp;Devs</h1>
 
-                </a>
-            </li>
+				</a>
+			</li>
 
-        </ul>
+		</ul>
 
-        <nav class='main-menu'>
-            <ul>
+		<nav class='main-menu'>
+			<ul>
 
-                <li><a href="?page=home" class=" <?php activePage("home") ?>">Home</a></li>
-                <li><a href="?page=about" class=" <?php activePage("about") ?>">About</a></li>
-                <li><a href="?page=projects" class=" <?php activePage("projects") ?>">Projects</a></li>
-                <li class="large-size">
-                    <a href="?page=writing " class="<?php activePage("writing") ?>">Writing</a>
-                </li>
-                <li class="larger-size">
-                    <a href="?page=forms " class="<?php activePage("forms") ?>">Forms</a>
-                </li>
-                <li class="larger-size">
-                    <a href="?page=style " class="<?php activePage("style") ?>">Guide</a>
-                </li>
-            </ul>
+				<li><a href="?page=home" class=" <?php activePage("home") ?>">Home</a></li>
+				<li><a href="?page=about" class=" <?php activePage("about") ?>">About</a></li>
+				<li><a href="?page=projects" class=" <?php activePage("projects") ?>">Projects</a></li>
+				<li class="large-size">
+					<a href="?page=writing " class="<?php activePage("writing") ?>">Writing</a>
+				</li>
+				<li class="larger-size">
+					<a href="?page=forms " class="<?php activePage("forms") ?>">Forms</a>
+				</li>
+				<li class="larger-size">
+					<a href="?page=style " class="<?php activePage("style") ?>">Guide</a>
+				</li>
+			</ul>
 
-        </nav>
+		</nav>
 
-        <button class='hamburger-button' id='toggle' rel='hamburger'>
-            <div class='menu-bars'></div>
-        </button>
-
-
-        <nav class="burger">
-            <a class="small-size" href="?page=home" class=" <?php activePage("home") ?>">Home</a>
-            <a class="small-size" href="?page=about" class=" <?php activePage("about") ?>">About</a>
-            <a class="small-size" href="?page=projects" class=" <?php activePage("projects") ?>">Projects</a>
-            <a href="?page=writing " class="<?php activePage("writing") ?>">Writing</a>
-
-            <a href="?page=forms" class="<?php activePage("forms") ?>">Forms</a>
-            <a href="?page=style " class="<?php activePage("style") ?>">Guide</a>
-        </nav>
+		<button class='hamburger-button' id='toggle' rel='hamburger'>
+			<div class='menu-bars'></div>
+		</button>
 
 
+		<nav class="burger">
+			<a class="small-size" href="?page=home" class=" <?php activePage("home") ?>">Home</a>
+			<a class="small-size" href="?page=about" class=" <?php activePage("about") ?>">About</a>
+			<a class="small-size" href="?page=projects" class=" <?php activePage("projects") ?>">Projects</a>
+			<a href="?page=writing " class="<?php activePage("writing") ?>">Writing</a>
 
-    </inner-column>
+			<a href="?page=forms" class="<?php activePage("forms") ?>">Forms</a>
+			<a href="?page=style " class="<?php activePage("style") ?>">Guide</a>
+		</nav>
+
+
+
+	</inner-column>
 </header>
