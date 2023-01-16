889941cad5e606d6e5edc0f07d271fd60ecaa1d9
Jesse <jessemerge2@gmail.com>
2023-01-15 18:07:22 -0800

    add back

---
+++ templates/modules/site-menu/template.php
@@ -0,0 +1,62 @@
+<header class='main-tool-bar site-header'>
+	<inner-column>
+		<ul class='logo-container'>
+			<li class="svg-box">
+				<div class="container">
+					<div class="cube">
+						<div class="face top"></div>
+						<div class="face bottom"></div>
+						<div class="face left"></div>
+						<div class="face right"></div>
+						<div class="face front"></div>
+						<div class="face back"></div>
+					</div>
+				</div>
+			</li>
+			<li>
+				<a class='logo-link' href="">
+					<h1 class="logo">Jesse&nbsp;Devs</h1>
+
+				</a>
+			</li>
+
+		</ul>
+
+		<nav class='main-menu'>
+			<ul>
+
+				<li><a href="" class=" <?php activePage("") ?>">Home</a></li>
+				<li><a href="about" class=" <?php activePage("about") ?>">About</a></li>
+				<li><a href="projects" class=" <?php activePage("projects") ?>">Projects</a></li>
+				<li class="large-size">
+					<a href="writing" class="<?php activePage("writing") ?>">Writing</a>
+				</li>
+				<li class="larger-size">
+					<a href="forms" class="<?php activePage("forms") ?>">Forms</a>
+				</li>
+				<li class="larger-size">
+					<a href="style" class="<?php activePage("style") ?>">Guide</a>
+				</li>
+			</ul>
+
+		</nav>
+
+		<button class='hamburger-button' id='toggle' rel='hamburger'>
+			<div class='menu-bars'></div>
+		</button>
+
+
+		<nav class="burger">
+			<a class="small-size" href="" class=" <?php activePage("") ?>">Home</a>
+			<a class="small-size" href="about" class=" <?php activePage("about") ?>">About</a>
+			<a class="small-size" href="projects" class=" <?php activePage("projects") ?>">Projects</a>
+			<a href="writing" class="<?php activePage("writing") ?>">Writing</a>
+
+			<a href="forms" class="<?php activePage("forms") ?>">Forms</a>
+			<a href="style" class="<?php activePage("style") ?>">Guide</a>
+		</nav>
+
+
+
+	</inner-column>
+</header>
