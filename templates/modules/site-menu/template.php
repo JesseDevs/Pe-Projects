d85fa31246b4086e9545f9510e952604fd87953b
Jesse <jessemerge2@gmail.com>
2023-01-15 17:37:05 -0800

    projects rename for file routing

--- templates/modules/site-menu/template.php
+++ templates/modules/site-menu/template.php
@@ -14,7 +14,7 @@
 				</div>
 			</li>
 			<li>
-				<a class='logo-link' href="?page=home">
+				<a class='logo-link' href="">
 					<h1 class="logo">Jesse&nbsp;Devs</h1>
 
 				</a>
@@ -25,17 +25,17 @@
 		<nav class='main-menu'>
 			<ul>
 
-				<li><a href="?page=home" class=" <?php activePage("home") ?>">Home</a></li>
-				<li><a href="?page=about" class=" <?php activePage("about") ?>">About</a></li>
-				<li><a href="?page=projects" class=" <?php activePage("projects") ?>">Projects</a></li>
+				<li><a href="" class=" <?php activePage("") ?>">Home</a></li>
+				<li><a href="about" class=" <?php activePage("about") ?>">About</a></li>
+				<li><a href="projects" class=" <?php activePage("projects") ?>">Projects</a></li>
 				<li class="large-size">
-					<a href="?page=writing " class="<?php activePage("writing") ?>">Writing</a>
+					<a href="writing " class="<?php activePage("writing") ?>">Writing</a>
 				</li>
 				<li class="larger-size">
-					<a href="?page=forms " class="<?php activePage("forms") ?>">Forms</a>
+					<a href="forms " class="<?php activePage("forms") ?>">Forms</a>
 				</li>
 				<li class="larger-size">
-					<a href="?page=style " class="<?php activePage("style") ?>">Guide</a>
+					<a href="style " class="<?php activePage("style") ?>">Guide</a>
 				</li>
 			</ul>
 
@@ -47,13 +47,13 @@
 
 
 		<nav class="burger">
-			<a class="small-size" href="?page=home" class=" <?php activePage("home") ?>">Home</a>
-			<a class="small-size" href="?page=about" class=" <?php activePage("about") ?>">About</a>
-			<a class="small-size" href="?page=projects" class=" <?php activePage("projects") ?>">Projects</a>
-			<a href="?page=writing " class="<?php activePage("writing") ?>">Writing</a>
+			<a class="small-size" href="" class=" <?php activePage("") ?>">Home</a>
+			<a class="small-size" href="about" class=" <?php activePage("about") ?>">About</a>
+			<a class="small-size" href="projects" class=" <?php activePage("projects") ?>">Projects</a>
+			<a href="writing " class="<?php activePage("writing") ?>">Writing</a>
 
-			<a href="?page=forms" class="<?php activePage("forms") ?>">Forms</a>
-			<a href="?page=style " class="<?php activePage("style") ?>">Guide</a>
+			<a href="forms" class="<?php activePage("forms") ?>">Forms</a>
+			<a href="style " class="<?php activePage("style") ?>">Guide</a>
 		</nav>
 
 
