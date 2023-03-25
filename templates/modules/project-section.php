365a0eda74a736ec6c63e13a0b6f83e4dd833ae6
Jesse <jessemerge2@gmail.com>
2023-03-25 02:50:09 -0700

    Redo entire concept for project card

--- templates/modules/project-section.php
+++ templates/modules/project-section.php
@@ -1,42 +1,57 @@
-<project-section>
+<script>
+	let currentIndex = 0;
+	var projectsArray = <?= json_encode($projectsData); ?>;
+	console.log(projectsArray);
+
+	function reloadCard() {
+		const projectCard = document.querySelector('project-card');
+
+		projectCard.innerHTML = `
+			<picture class='project-thumbnail'>
+				<img src="${projectsArray[currentIndex].thumbnail}" alt="" loading='lazy'>
+							<button id="next-project-btn">&#10132;</button>
+			</picture>
+			<text-content>
+				<h3 class='chant-voice'>${projectsArray[currentIndex].title}</h3>
+				<span class="span-title">${projectsArray[currentIndex]['intro']}</span>
+				<p class="project-detail ">${projectsArray[currentIndex].detail}</p>
+			</text-content>
+			<div class="skills">
+				${projectsArray[currentIndex].skills.map(skill => `<p class="small-voice">${skill}</p>`).join('')}
+			</div>
+		
+			<a class="action-link" href="${projectsArray[currentIndex].links[0].link}">Click!</a>
+
+
+		`;
+	}
+
+	window.addEventListener('DOMContentLoaded', function() {
+		reloadCard();
+	});
+
+	window.addEventListener('click', function(event) {
+		if (event.target.matches('#next-project-btn')) {
+			loadNextProject();
+			reloadCard();
+		}
+	});
+
+	function loadNextProject() {
+		currentIndex++;
+
+		if (currentIndex >= projectsArray.length) {
+			currentIndex = 0;
+		}
+		console.log(currentIndex)
+	}
+</script>
+
+<project-section class='grid-template'>
 	<text-content>
 		<h2 class='loud-voice'><?= $section['heading']; ?></h2>
-
 		<p class='intro'><?= $section['intro']; ?></p>
 	</text-content>
 
-	<div class="swiper mySwiper">
-		<div class="swiper-wrapper">
-
-			<?php
-			if ($page == 'home') {
-				foreach ($projectsData as $project) { ?>
-					<?php if ($project['featured']) { ?>
-						<div class="swiper-slide">
-							<?php include('templates/modules/project-card.php'); ?>
-						</div>
-			<?php }
-				}
-			} ?>
-
-			<?php if ($page == 'projects' or $page == 'style') {
-				foreach ($projectsData as $project) { ?>
-					<div class="swiper-slide">
-						<?php include('templates/modules/project-card.php'); ?>
-					</div>
-			<?php }
-			}
-			?>
-		</div>
-		<!-- If we need pagination -->
-		<div class="swiper-pagination"></div>
-
-		<!-- If we need navigation buttons -->
-		<div class="swiper-button-prev"></div>
-		<div class="swiper-button-next"></div>
-
-	</div>
-	</div>
-	</div>
-
+	<project-card></project-card>
 </project-section>
