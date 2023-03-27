997eeaf636fafb75671d7d0b7ff355be982255ca
Jesse <jessemerge2@gmail.com>
2023-03-27 01:12:41 -0700

    Working on about page resume design

--- templates/modules/resume-section.php
+++ templates/modules/resume-section.php
@@ -1,20 +1,17 @@
-<resume-section id="resume">
-  
- <text-content>
-   <h3 class='attention-voice'><?= $section['heading']; ?></h3>
+<resume-section id="resume" class='grid-template'>
+
+	<text-content>
+		<h2 class='loud-voice'><?= $section['heading']; ?></h2>
+		<p class='intro'><?= $section['intro']; ?></p>
+	</text-content>
 
 
-   <p class='intro'><?= $section['intro']; ?></p>
- </text-content>
+	<h4 class='chant-voice'>Work History</h4>
+	<?php renderList($resumeData, "job"); ?>
 
 
- <h4 class='chant-voice'>Work History</h4>
- <?php renderList($resumeData, "job"); ?>
-
-
- <h4 class='chant-voice education'>Education History</h4>
- <?php renderList($resumeData, "education"); ?>
-
+	<h4 class='chant-voice education'>Education History</h4>
+	<?php renderList($resumeData, "education"); ?>
 
 
 </resume-section>
