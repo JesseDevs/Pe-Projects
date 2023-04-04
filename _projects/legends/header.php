587b9e44ac5b5a8998cd6f6b01d676ebde1165f8
Jesse <jessemerge2@gmail.com>
2023-04-04 01:31:28 -0700

    Touch up legends

--- _projects/legends/header.php
+++ _projects/legends/header.php
@@ -2,35 +2,35 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Legends</title>
-    <meta name='description' content="Apex Legends descriptions">
-    <meta property="og:image" content="https://peprojects.dev/alpha-4/jesse/projects/legends/images/metainfo.png">
+	<title>Legends</title>
+	<meta name='description' content="Apex Legends descriptions">
+	<meta property="og:image" content="https://peprojects.dev/alpha-4/jesse/projects/legends/images/metainfo.png">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/style.css">
 
 </head>
 
 <body>
 
-    <header class="doc-header">
-        <inner-column>
-            <nav>
+	<header class="doc-header">
+		<inner-column>
+			<nav>
 
-                <picture>
-                    <img src="images/apex-white-icon.svg" alt="">
-                </picture>
+				<picture>
+					<img src="images/apex-white-icon.svg" alt="">
+				</picture>
 
-                <h1>MEET THE LEGENDS</h1>
+				<h1 class='chant-voice'>MEET THE LEGENDS</h1>
 
-                <p>It takes grit, talent, and a lot of luck to become a legend. Pick your favorite and see what their unique set of skills can do for your squad.</p>
+				<p>It takes grit, talent, and a lot of luck to become a legend. Pick your favorite and see what their unique set of skills can do for your squad.</p>
 
-            </nav>
-        </inner-column>
-    </header>
+			</nav>
+		</inner-column>
+	</header>
